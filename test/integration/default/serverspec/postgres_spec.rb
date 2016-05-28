9a35c8cfecab8f3568187e169f9e5f675bac377b
Javier Bértoli <javier@netmanagers.com.ar>
2016-05-28 09:27:38 -0300

    Avoid invalid YAML creation (thanks, @nicksloan !)
    * Reordered resources and requires
    * Added tests for CentOS platform

--- test/integration/default/serverspec/postgres_spec.rb
+++ test/integration/default/serverspec/postgres_spec.rb
@@ -1,4 +1,4 @@
-require "serverspec"
+require_relative '../../../kitchen/data/spec_helper'
 
 describe service("postgresql") do
   it { should be_enabled }
@@ -11,7 +11,7 @@ end
 
 describe file("/srv/my_tablespace") do
   it { should be_directory }
-  it { should be_mode 755 }
+  it { should be_mode 700 }
   it { should be_owned_by "postgres" }
   it { should be_grouped_into "postgres" }
 end

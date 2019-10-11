b369aa9d40b3f686504876d231ce05ec05fb90ee
Imran Iqbal <iqbalmy@hotmail.com>
2019-10-11 12:52:29 +0100

    fix(rubocop): fix remaining errors manually

--- test/integration/default/controls/command_spec.rb
+++ test/integration/default/controls/command_spec.rb
@@ -9,6 +9,10 @@ control 'Postgres command' do
 
   # Can't use `%Q` here due to the `\`
   describe command("su - postgres -c 'psql -p" + pg_port + %q( -qtc "\l+ db2"')) do
-    its(:stdout) { should match(/db2.*remoteUser.*UTF8.*en_US.UTF-8.*en_US.UTF-8.*my_space/) }
+    its(:stdout) do
+      should match(
+        /db2.*remoteUser.*UTF8.*en_US.UTF-8.*en_US.UTF-8.*my_space/
+      )
+    end
   end
 end
